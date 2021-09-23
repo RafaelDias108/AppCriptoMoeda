20ce7bf24c43efab439a6454be8bf947a0b60449
rafaeld108 <38574440+rafaeld108@users.noreply.github.com>
2021-09-23 12:48:01 -0300

    Criado um form para receber o valor de compra da moeda, criado o botão com as validações e mostrar a quantidade que está sendo comprada de acordo com o valor da moeda escolhida.

--- lib/pages/moedas_detalhes_page.dart
+++ lib/pages/moedas_detalhes_page.dart
@@ -1,5 +1,6 @@
 import 'package:cripto_moedas_app/models/moeda.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
 
 class MoedasDetalhesPage extends StatefulWidget {
@@ -13,6 +14,26 @@ class MoedasDetalhesPage extends StatefulWidget {
 
 class _MoedasDetalhesPageState extends State<MoedasDetalhesPage> {
   NumberFormat real = NumberFormat.currency(locale: 'pt_BR', name: 'R\$');
+
+  final _form = GlobalKey<FormState>();
+  final _valor = TextEditingController();
+
+  double quantidade = 0;
+
+  comprar() {
+    if (_form.currentState!.validate()) {
+      // salvar a compra
+      Navigator.pop(context);
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+            content: Text(
+          'Compra relizada com sucesso !',
+          textAlign: TextAlign.center,
+        )),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,6 +66,77 @@ class _MoedasDetalhesPageState extends State<MoedasDetalhesPage> {
                     ),
                   ),
                 ],
+              ),
+            ),
+            (quantidade > 0)
+                ? SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    child: Container(
+                      child: Text(
+                        '$quantidade ${widget.moeda.sigla}',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.teal,
+                        ),
+                      ),
+                      margin: EdgeInsets.only(bottom: 24),
+                      alignment: Alignment.center,
+                    ),
+                  )
+                : Container(
+                    margin: EdgeInsets.only(bottom: 54),
+                  ),
+            Form(
+              key: _form,
+              child: TextFormField(
+                controller: _valor,
+                style: TextStyle(fontSize: 22),
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Valor',
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  suffix: Text(
+                    'reais',
+                    style: TextStyle(fontSize: 14),
+                  ),
+                ),
+                keyboardType: TextInputType.number,
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Informe o valor da compra";
+                  } else if (double.parse(value) < 50) {
+                    return "Compra mínima é R\$ 50,00";
+                  }
+                  return null;
+                },
+                onChanged: (value) {
+                  setState(() {
+                    quantidade = (value.isEmpty)
+                        ? 0
+                        : double.parse(value) / widget.moeda.preco;
+                  });
+                },
+              ),
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              margin: EdgeInsets.only(top: 24),
+              child: ElevatedButton(
+                onPressed: comprar,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.check),
+                    Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Text(
+                        'Comprar',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ],
